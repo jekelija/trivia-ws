f667ebd30a97ed3bb02d553974b7a31cf41d3811
Jon Jekeli <jon@marxent.com>
2020-02-24 10:15:54 -0500

    udpated questions

--- README.md
+++ README.md
@@ -10,4 +10,7 @@ what if peoples phones go to sleep
 march madness
 wildcats vs wildcats
 
+
+round 2, brotherly love (seth curry and steph currey top 10 in career 3pt %)
 final jeopardy: last game winner as time expires (austin rivers)
+
--- server/src/games/basketball.ts
+++ server/src/games/basketball.ts
@@ -37,13 +37,13 @@ export const BasketballGame: Game = {
             questions: [
                 {
                     value: 100,
-                    question: "Uttered after Brink twists his ankle",
-                    answer: "Hey man, you ok?"
+                    question: "Most wins in a season",
+                    answer: "Golden state (with 73)"
                 },
                 {
                     value: 200,
-                    question: "Said to justify taking social dance class",
-                    answer: "It just fit into my schedule"
+                    question: "All Time Leader in NBA Assists",
+                    answer: "John Stockton"
                 },
                 {
                     value: 300,
@@ -52,13 +52,13 @@ export const BasketballGame: Game = {
                 },
                 {
                     value: 400,
-                    question: "The name of hurricane on the announcements when jon ran naked into the apartment and bit it",
-                    answer: "Hurricane Tiger Geiger"
+                    question: "This player has led the NBA in rebounds 3 out of the last 4 years",
+                    answer: "Andre Drummond"
                 },
                 {
                     value: 500,
-                    question: "Phrase yelled from underneath Jon's bed in a successful attempt to scare the crap out of him",
-                    answer: "Candy Bars!"
+                    question: "Lebron James is the only active player in the top 10 of 6 major statistical categories; name 3",
+                    answer: "Turnovers, Field Goals Made, Field Goals Attempted, Minutes, Points, Free Throws Made (Dwight Howard also in FT attempted) (chris paul in assists)"
                 }
             ]
         },
@@ -97,28 +97,28 @@ export const BasketballGame: Game = {
             questions: [
                 {
                     value: 100,
-                    question: "Roy Williams",
-                    answer: "UNC, Kansas"
+                    question: "This city has hosted the First Four every year since its inception in 2011",
+                    answer: "Dayton"
                 },
                 {
                     value: 200,
-                    question: "Dwyane Wade",
-                    answer: "Miami Heat, Chicago Bulls, Cleveland Cavs"
+                    question: "In 1976, this was the last team to finish the season undefeated",
+                    answer: "Indiana Hoosiers"
                 },
                 {
                     value: 300,
-                    question: "Kyrie Irving",
-                    answer: "Cleveland Cavs, Boston Celtics, New Jersey Nets"
+                    question: "Most NCAA Championships",
+                    answer: "UCLA"
                 },
                 {
                     value: 400,
-                    question: "Steve Nash",
-                    answer: "Phoenix Suns, Dallas Mavericks, Los Angeles Lakers"
+                    question: "This team was the first 16 seed to beat a 1 seed",
+                    answer: "UMBC (vs virginia)"
                 },
                 {
                     value: 500,
-                    question: "Grant Hill",
-                    answer: "Detroit Pistons, Orlando Magic, Phoenix Suns, Los Angeles Clippers"
+                    question: "How many teams in the first NCAA tournament",
+                    answer: "8"
                 }
             ]
         },
@@ -127,27 +127,27 @@ export const BasketballGame: Game = {
             questions: [
                 {
                     value: 100,
-                    question: "Roy Williams",
+                    question: "This Hall of Fame Laker led the league in assists 4 times",
                     answer: "Magic Johnson"
                 },
                 {
                     value: 200,
-                    question: "Dwyane Wade",
+                    question: "This university produced several NBA players, including Markelle Fultz, Terrance Ross, and Spencer Hawes",
                     answer: "Washington University"
                 },
                 {
                     value: 300,
-                    question: "Kyrie Irving",
+                    question: "This coach is known as the father of the triangle offense and the Zen Master",
                     answer: "Phil Jackson"
                 },
                 {
                     value: 400,
-                    question: "Steve Nash",
+                    question: "Sharing a last name with the 14th president, this player won a championship in Boston in 2008",
                     answer: "Paul Pierce"
                 },
                 {
                     value: 500,
-                    question: "Grant Hill",
+                    question: "Sharing a name with the 5th president, this lefty center attended Georgetown and has averaged 13 points and 8 rebounds a game over his 9 year career",
                     answer: "Greg Monroe"
                 }
             ]
