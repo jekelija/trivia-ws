9f5609b9ff4a8e738e4973be8d28c8b468b74c90
jjwyse <joshua.wyse@gmail.com>
2020-04-11 08:40:10 -0600

    Change websocket connection URL and protocol

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,4 @@
 export const PROTOCOL = "https://";
-export const HOST = "4ed325cc.ngrok.io/";
-export const URL = "wss://" + HOST;
+export const HOST = "localhost:8081";
+export const URL = "ws://" + HOST;
 export const GAME_TYPE = "basketball";
