965b61438e89f84416136c24251e1b7915e421ae
Jon Jekeli <jon@marxent.com>
2020-04-19 18:27:06 -0400

    update ngrok

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,4 @@
 export const PROTOCOL = "https://";
-export const HOST = "9388dbf7.ngrok.io/";
+export const HOST = "c359c9d4.ngrok.io/"; 
 export const URL = "wss://" + HOST;
 export const GAME_TYPE = "housechurch";
