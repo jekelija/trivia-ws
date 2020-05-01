c1357ebc87d4554b28fe24b8fa57c2e3d15d2145
Jon Jekeli <jon@marxent.com>
2020-04-30 21:11:12 -0400

    update host

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,4 @@
 export const PROTOCOL = "https://";
-export const HOST = "64fd737e.ngrok.io/"; 
+export const HOST = "04df0b0e.ngrok.io/"; 
 export const URL = "wss://" + HOST;
 export const GAME_TYPE = "friends";
